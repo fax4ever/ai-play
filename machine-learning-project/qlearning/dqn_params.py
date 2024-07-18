7c37a77c851e312c3e50b27619a01a850608440e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-07-18 22:02:47 +0200

    Finalize the document and the project
    Still need to review the last chapter --- but I'm almost done

--- machine-learning-project/qlearning/dqn_params.py
+++ machine-learning-project/qlearning/dqn_params.py
@@ -1,12 +1,12 @@
 class DQN_PARAMS:
     def __init__(self):
-        self.learning_rate = 0.1 # (alpha0)
-        self.discount_factor = 0.95 # (gamma)
-        self.replay_buffer_size = 50_000
+        self.learning_rate = 0.3 # (alpha0)
+        self.discount_factor = 0.99 # (gamma)
+        self.replay_buffer_size = 35_000
         self.min_replay_buffer_size = 1_000 # keep this value grater or equal to mini_batch_size
         self.mini_batch_size = 128
         self.train_episodes = 300
-        self.steps_to_update_policy_model = 4
+        self.steps_to_update_policy_model = 2
         self.steps_to_update_target_model = 100
         self.network_learning_rate = 0.001 # (for the gradient descent)
         self.max_episode_steps = 500
