7b1f1180f1559653172c5cb362b020181cb978d1
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-06-06 14:33:11 +0200

    Add on demand feature

--- feast-play/custom_repo/custom_repo.py
+++ feast-play/custom_repo/custom_repo.py
@@ -1,12 +1,16 @@
 from datetime import timedelta
 
+import pandas as pd
+
 from feast.types import Float32, Float64, Int64
+from feast.on_demand_feature_view import on_demand_feature_view
 
 from feast import (
     Entity,
     FeatureView,
     Field,
     FileSource,
+    RequestSource,
 )
 
 
@@ -47,3 +51,29 @@ driver_stats_fv = FeatureView(
     # feature view
     tags={"team": "driver_performance"},
 )
+
+# Define a request data source which encodes features / information only
+# available at request time (e.g. part of the user initiated HTTP request)
+input_request = RequestSource(
+    name="vals_to_add",
+    schema=[
+        Field(name="val_to_add", dtype=Int64),
+        Field(name="val_to_add_2", dtype=Int64),
+    ],
+)
+
+
+# Define an on demand feature view which can generate new features based on
+# existing feature views and RequestSource features
+@on_demand_feature_view(
+    sources=[driver_stats_fv, input_request],
+    schema=[
+        Field(name="conv_rate_plus_val1", dtype=Float64),
+        Field(name="conv_rate_plus_val2", dtype=Float64),
+    ],
+)
+def transformed_conv_rate(inputs: pd.DataFrame) -> pd.DataFrame:
+    df = pd.DataFrame()
+    df["conv_rate_plus_val1"] = inputs["conv_rate"] + inputs["val_to_add"]
+    df["conv_rate_plus_val2"] = inputs["conv_rate"] + inputs["val_to_add_2"]
+    return df
--- feast-play/custom_repo/main_app.py
+++ feast-play/custom_repo/main_app.py
@@ -6,7 +6,12 @@ from datetime import datetime
 
 def main():
     store = FeatureStore(repo_path=".")
+    simple_enrich(store)
+    transformation_enrich(store)
+    transformation_enrich_inference(store)
 
+
+def simple_enrich(store):
     entity_df = pd.DataFrame.from_dict(
         {
             # entity's join key -> entity values
@@ -21,13 +26,71 @@ def main():
             "label_driver_reported_satisfaction": [1, 5, 3],
         }
     )
-
     training_df = store.get_historical_features(
         entity_df=entity_df,
         features=[
             "driver_hourly_stats:conv_rate",
             "driver_hourly_stats:acc_rate",
             "driver_hourly_stats:avg_daily_trips"
+        ],
+    ).to_df()
+    print(training_df.head())
+
+
+def transformation_enrich(store):
+    entity_df = pd.DataFrame.from_dict(
+        {
+            # entity's join key -> entity values
+            "driver_id": [1001, 1002, 1003],
+            # "event_timestamp" (reserved key) -> timestamps
+            "event_timestamp": [
+                datetime(2021, 4, 12, 10, 59, 42),
+                datetime(2021, 4, 12, 8, 12, 10),
+                datetime(2021, 4, 12, 16, 40, 26),
+            ],
+            # (optional) label name -> label values. Feast does not process these
+            "label_driver_reported_satisfaction": [1, 5, 3],
+            # values we're using for an on-demand transformation
+            "val_to_add": [1, 2, 3],
+            "val_to_add_2": [10, 20, 30],
+        }
+    )
+    training_df = store.get_historical_features(
+        entity_df=entity_df,
+        features=[
+            "driver_hourly_stats:conv_rate",
+            "driver_hourly_stats:acc_rate",
+            "driver_hourly_stats:avg_daily_trips",
+            "transformed_conv_rate:conv_rate_plus_val1",
+            "transformed_conv_rate:conv_rate_plus_val2",
+        ],
+    ).to_df()
+    print(training_df.head())
+
+
+def transformation_enrich_inference(store):
+    entity_df = pd.DataFrame.from_dict(
+        {
+            # entity's join key -> entity values
+            "driver_id": [1001, 1002, 1003],
+            # (optional) label name -> label values. Feast does not process these
+            "label_driver_reported_satisfaction": [1, 5, 3],
+            # values we're using for an on-demand transformation
+            "val_to_add": [1, 2, 3],
+            "val_to_add_2": [10, 20, 30],
+        }
+    )
+
+    entity_df["event_timestamp"] = pd.to_datetime("now", utc=True)
+
+    training_df = store.get_historical_features(
+        entity_df=entity_df,
+        features=[
+            "driver_hourly_stats:conv_rate",
+            "driver_hourly_stats:acc_rate",
+            "driver_hourly_stats:avg_daily_trips",
+            "transformed_conv_rate:conv_rate_plus_val1",
+            "transformed_conv_rate:conv_rate_plus_val2",
         ],
     ).to_df()
     print(training_df.head())
