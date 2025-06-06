926dda026f4e368aba82956725427117b8dfae23
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-06-06 13:51:26 +0200

    Add feature view

--- feast-play/custom_repo/main_app.py
+++ feast-play/custom_repo/main_app.py
@@ -1,8 +1,36 @@
 from feast import FeatureStore
 
+import pandas as pd
+from datetime import datetime
+
 
 def main():
-    FeatureStore(repo_path=".")
+    store = FeatureStore(repo_path=".")
+
+    entity_df = pd.DataFrame.from_dict(
+        {
+            # entity's join key -> entity values
+            "driver_id": [1001, 1002, 1003],
+            # "event_timestamp" (reserved key) -> timestamps
+            "event_timestamp": [
+                datetime(2021, 4, 12, 10, 59, 42),
+                datetime(2021, 4, 12, 8, 12, 10),
+                datetime(2021, 4, 12, 16, 40, 26),
+            ],
+            # (optional) label name -> label values. Feast does not process these
+            "label_driver_reported_satisfaction": [1, 5, 3],
+        }
+    )
+
+    training_df = store.get_historical_features(
+        entity_df=entity_df,
+        features=[
+            "driver_hourly_stats:conv_rate",
+            "driver_hourly_stats:acc_rate",
+            "driver_hourly_stats:avg_daily_trips"
+        ],
+    ).to_df()
+    print(training_df.head())
 
 
 if __name__ == "__main__":
